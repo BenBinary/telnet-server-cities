39041f816825e7013a71152ef58d82680f4b2ac7
BenBinary <43401683+BenBinary@users.noreply.github.com>
2022-01-03 23:05:32 +0200

    vv

---
+++ src/Cityclass.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author FHBBook
+ */
+public class Cityclass {
+    
+    int orderNumber;
+    String city_nom;
+    String city_gen;
+    String city_acc;
+    String city_dat;
+    String city_english;
+    String city_state;
+
+    public Cityclass(int orderNumber, String city_nom, String city_gen, String city_acc, String city_dat, String city_english, String city_state) {
+        this.orderNumber = orderNumber;
+        this.city_nom = city_nom;
+        this.city_gen = city_gen;
+        this.city_acc = city_acc;
+        this.city_dat = city_dat;
+        this.city_english = city_english;
+        this.city_state = city_state;
+    }
+
+    @Override
+    public String toString() {
+        return city_nom + " " + city_gen + " " + city_acc + " " + city_dat + " " + city_english + " " + city_state;
+    }
+    
+    
+    
+    
+    
+    
+}
---
+++ src/TranslationServer.java
@@ -0,0 +1,163 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TranslationServer {
+    
+    private static ArrayList<Cityclass> ACL = new ArrayList<>();
+    private static HashMap<String, String> Translations = new HashMap<String, String>();
+    static {
+        Translations.put("car", "blub");
+        Translations.put("drive", "οδηγώ");
+        Translations.put("fruits", "φρούτα");
+        Translations.put("orange", "πορτοκάλι");
+        Translations.put("apple", "μήλο");
+        Translations.put("apricot", "βερίκοκο");
+        Translations.put("pear", "αχλάδι");
+        Translations.put("vegetables", "λαχανικά");
+        Translations.put("tomato", "ντομάτα");
+        Translations.put("eggplant", "μελιτζάνα");
+        Translations.put("meat", "κρέας");
+        Translations.put("pork", "χοιρινό");
+        Translations.put("veal", "μοσχάρι");
+        Translations.put("beef", "μοσχάρι");
+        Translations.put("eat", "τρώγω");
+    }
+    
+    private static String get_translation(String original) {
+        return Translations.get(original);
+    }
+    
+    /**
+     * Fills the array with cities by reading it from the cities.txt file
+     */
+    private static void fill_cities() {
+        
+        try {
+             
+            File cityfile = new File("res/cities.txt");
+            
+            if (cityfile.exists()) {
+                System.out.println("it exists");
+            }
+            System.out.println("File " + cityfile.getAbsolutePath());
+            // C:\Users\FHBBook\Documents\NetBeansProjects\SrvSocket1
+            
+            
+            Scanner myreader = new Scanner(cityfile);
+            
+            while (myreader.hasNext()) {
+                
+                String raw = myreader.nextLine();
+                String[] arr = raw.split("#");
+                
+                System.out.println(arr[1]);
+                Cityclass city = new Cityclass(Integer.valueOf(arr[0]), arr[1], arr[2], arr[3], arr[4], arr[5], arr[6]);    
+                ACL.add(city);
+                
+            } 
+            
+            myreader.close(); 
+            
+        } catch (FileNotFoundException fnfe) {
+            
+            System.out.println(fnfe.getLocalizedMessage());
+            
+        } 
+     
+        
+    }
+    
+    private static String get_city(String city) {
+     
+
+        for (Cityclass singleCity : ACL) {
+            
+            if (singleCity.city_nom.matches(city)) {
+                return singleCity.toString();
+            } 
+        }
+     
+        return "";
+    }
+
+    public static int ECHOPORT = 6789;
+    
+    public static void main(String argv[]) {
+
+        fill_cities();
+        
+        ServerSocket s = null;
+        try {
+            s = new ServerSocket(ECHOPORT);
+        } catch(IOException e) {
+            System.out.println(e);
+            System.exit(1);
+        }
+        while (true) {
+            Socket incoming = null;
+            try {
+                incoming = s.accept();
+            } catch (IOException e) {
+                System.out.println("IOException while accept " + e);
+                continue;
+            }
+            try {
+                handleSocket(incoming);
+            } catch(IOException e) {
+                System.out.println("IOException when handleSocket " + e);
+            }
+            try {
+                incoming.close();
+            } catch(IOException e) {
+                // no message required
+            }
+        }
+    }
+    
+    public static void handleSocket(Socket incoming) throws IOException {
+        
+        BufferedReader reader =
+            new BufferedReader(new InputStreamReader(
+            incoming.getInputStream()));
+        
+        PrintStream out = new PrintStream(incoming.getOutputStream(),true,"cp850");    
+
+        out.println("Hello. Enter BYE to exit");
+        boolean done = false;
+        while ( ! done) {
+            String str = reader.readLine();
+            if (str == null) {
+                done = true;
+                System.out.println("Null received");
+            }
+            else if (str.matches("City.*")) {
+             
+                str = str.replace("City ", "");
+                
+                out.println("Translation: " + get_city(str));
+                
+                if (str.trim().equals("BYE"))
+                    done = true;
+              
+            }
+            else {
+                //out.println("Translation: " + get_translation(str));
+                // out.println("Translation: " + get_city(str));
+                
+                if (str.trim().equals("BYE"))
+                    done = true;
+            }
+        }
+        incoming.close();
+    }
+}
